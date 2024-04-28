d1fe09a921c42a1f629a0bd7a622062e1a7f6d5c
Varun Pereira <68728165+varunpereira@users.noreply.github.com>
2024-04-28 14:27:28 +1000

    d

--- app/pieces/footer.jsx
+++ app/pieces/footer.jsx
@@ -14,7 +14,6 @@ import {
 	V,
 	P,
 } from "~/config/shop"
-import logo from "~/pieces/logo.png"
 
 export default () => {
 	var nav = route()
@@ -22,7 +21,7 @@ export default () => {
 	return (
 		<D style={() => "mx_auto a_row ax_equal v2:px-[1rem] v3:px-[2rem] v4:px-[2.5rem] v5:px-[3rem] "}>
 			<B click={() => nav("/")} style={() => "tc_1 tw_1 ts_3"}>
-				<P value={() => logo} style={() => `w-[4rem] `} />
+				<img src={'/logo.png'} class={`w-[6rem]`} />
 			</B>
 			<T style={() =>`ts_1`}>© 2024 Netflix - Varun Pereira</T>
 		</D>
