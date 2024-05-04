ca1c49b2fc9b19f82b51db8653235525b332559e
Varun Pereira <68728165+varunpereira@users.noreply.github.com>
2024-05-04 21:16:32 +1000

    d

--- app/results.jsx
+++ app/results.jsx
@@ -31,18 +31,25 @@ export default () => {
 		page.title = `Search Results - Netflix`
 		term(path_par?.q.trim().toUpperCase())
 		shows_filt(all_shows_data().filter((show) => show?.keywords.toUpperCase().includes(term())))
- 	})
+	})
 
 	return (
 		<div class="pt-[10rem] v2:px-[1rem] v3:px-[2rem] v4:px-[2.5rem] v5:px-[3rem]">
 			<D style={() => `w-full h-full a_col gap-[4rem]`}>
 				<div class={`a_row_auto items-center justify-start gap-x-[.3rem] gap-y-[4rem] `}>
-					{shows_filt().length > 0 ? shows_filt()?.map((v,i) => (
-						<div class={`z_fit w-[520px] h-[260px] z-[${i+1}] `}>
-						<img src={v?.poster_link} class={`z_put w-[260px] h-[130px] aspect-[16/9] hover:w-[520px] hover:h-[260px] `} />
-
-						</div>
-					)) : <p>No results found for "{path_par?.q}"</p>}
+					{shows_filt().length > 0 ? (
+						shows_filt()?.map((v, i) => (
+							<div
+								class={`z_fit w-[14rem] h-[7rem] hover:w-[28rem] hover:mt-[-7rem]`}>
+								<img
+									src={v?.poster_link}
+									class={`z_put z-[0] w-full h-full aspect-[16/9] hover:h-[14rem] hover:z-[1] `}
+								/>
+							</div>
+						))
+					) : (
+						<p>No results found for "{path_par?.q}"</p>
+					)}
 				</div>
 			</D>
 		</div>
