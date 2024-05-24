542ebb1471b3bda7db2206e9282e6ac17b96e914
Varun Pereira <68728165+varunpereira@users.noreply.github.com>
2024-05-24 14:31:48 +1000

    d

--- app/pieces/nav.jsx
+++ app/pieces/nav.jsx
@@ -98,7 +98,7 @@ export default () => {
 							css={"transition: width 1s ease-in-out;"}
 							style={`ax_right v4:ax_right sx_mid border-white px-[.1rem] c_black mt-[1rem] v4:mt-[0rem] ${
 								!see_search()
-									? `w-0 h-0 border-0 overflow-hidden`
+									? `w-0 h-0 border-0 overflow-hidden hide`
 									: `w-full v4:w-[14rem] h-[1.7rem] border-[.1rem]`
 							}`}>
 							<B
