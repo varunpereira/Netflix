deb3cb0ba2b6e96ecc4bd6ae302abfa0a9c1ba8a
Varun Pereira <68728165+varunpereira@users.noreply.github.com>
2024-05-24 15:36:11 +1000

    d

--- app/pieces/nav.jsx
+++ app/pieces/nav.jsx
@@ -98,12 +98,12 @@ export default () => {
 							css={"transition: width 1s ease-in-out;"}
 							style={`ax_right sx_mid border-white px-[.1rem] c_black mt-[1rem] v4:mt-[0rem] 
 							${
-								// !see_search()
-								// 	? `w-0 h-0 border-0 overflow-hidden`
-								// 	: `w-full v4:w-[14rem] h-[1.7rem] border-[.1rem]`
-								`w-0 h-0 max-w-0 border-0 overflow-hidden`
+								!see_search()
+									? `w-0 h-0 max-w-0 border-0 overflow-hidden`
+									: `w-full v4:w-[14rem] h-[1.7rem] border-[.1rem]`
+								// `w-0 h-0 max-w-0 border-0 overflow-hidden`
 							}`
-							}> njjfkkker fernkfnkjnk
+							}> nljndlewkl ldkneld;j;
 							{/* <B
 								click={() => {
 									form_submit(form_data().search)
