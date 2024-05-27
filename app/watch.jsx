5a3a8c8f7c03588b3e8345fe73dd6a8da601042a
Varun Pereira <68728165+varunpereira@users.noreply.github.com>
2024-05-27 15:56:48 +1000

    d

--- app/watch.jsx
+++ app/watch.jsx
@@ -128,7 +128,7 @@ export default () => {
 
 			<D
 				style={`z_put z-[2] left-0 bottom-[3rem] ax_same w-full v2:px-[1rem] v3:px-[2rem] v4:px-[2.5rem] v5:px-[3rem]`}>
-				<D style="z_fit z-[2] w-full h-[.3rem]">
+				<D style="z_fit z-[2] w-full">
 					<D
 						style={`c_red z_put z-[3] h-[.3rem] w-full top-[.75rem]`}
 						css={`
